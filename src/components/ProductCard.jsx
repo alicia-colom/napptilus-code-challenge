45c756c8aab1e6a0094eeec0c83c6c7ed602609d
Alicia Colom <aliciacolom@gmail.com>
2022-07-20 23:14:35 +0200

    :construction:  + :lipstick:  + :iphone: Implement flex and responsiveness

--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -1,15 +1,17 @@
 import React from 'react';
+import { FlexBox } from 'react-styled-flex';
 
 const ProductCard = ({ product }) => {
 	return (
-		<>
-			<h3>{product.brand + ' ' + product.model}</h3>
+		<FlexBox column width='200px' center>
+			<h3>{product.brand}</h3>
+			<h4>{product.model}</h4>
 			<img src={product.imgUrl} alt={product.brand + ' ' + product.model} />
 			<div>
 				<small>{product.id}</small>
 				<div>{product.price} €</div>
 			</div>
-		</>
+		</FlexBox>
 	);
 };
 
