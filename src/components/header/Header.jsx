123593a22dd45860fb4b644a9e387fad2d90c500
Alicia Colom <aliciacolom@gmail.com>
2022-07-20 15:39:05 +0200

    :sparkles: + :lipstick:  Header component

---
+++ src/components/header/Header.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import "./Header.scss";
+
+function Header() {
+  return (
+    <header className="header headerContainer">
+      <div className="preHeader"></div>
+      <h1 className="headerTitle">Napptilus code challenge</h1>
+    </header>
+  );
+}
+
+export default Header;
