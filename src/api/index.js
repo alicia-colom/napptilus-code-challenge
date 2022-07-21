c81fcb7f1750cef34b19f67026f4dd997a460c47
Alicia Colom <aliciacolom@gmail.com>
2022-07-21 21:24:01 +0200

    :sparkles: Create call to consume ProductDetails and call to consume Cart

--- src/api/index.js
+++ src/api/index.js
@@ -9,3 +9,29 @@ export const getCatalogue = async () => {
 		throw error;
 	}
 };
+
+export const getProductDetail = async (idProduct) => {
+	try {
+		const response = await fetch(URL + 'product/' + idProduct);
+		console.log('*** Response getProductDetail:', response);
+		const data = await response.json();
+		return data;
+	} catch (error) {
+		throw error;
+	}
+};
+
+export const updateCart = async (productDetail) => {
+	try {
+		const response = await fetch(URL + 'cart', {
+			method: 'POST',
+			body: JSON.stringify(productDetail),
+			headers: { 'content-type': 'application/json' },
+		});
+		console.log('*** Response updateCart:', response);
+		const data = await response.json();
+		return data;
+	} catch (error) {
+		throw error;
+	}
+};
