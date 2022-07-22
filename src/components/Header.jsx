9c1a366280337c75278e5d37d79446fceb70bf5d
Alicia Colom <aliciacolom@gmail.com>
2022-07-22 23:14:37 +0200

    :lipstick: Update color variables and Update header and main

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,18 +1,28 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import { FlexBox } from 'react-styled-flex';
+import logoPhonestore from '../assets/phonestore-alfa.png';
 import '../styles/Header.scss';
 
 function Header({ cartCount }) {
   return (
     <FlexBox
       as="header"
-      className="headerContainer"
+      className="header"
       center
       justifyContent="space-between"
     >
-      <h1 className="headerTitle">Napptilus code challenge</h1>
-      <i className="fa fa-shopping-cart cart" aria-hidden="true">
-        <small className="cart__number">{cartCount}</small>
+      <Link to="/" className="header__heading">
+        <img
+          className="header__heading--logo"
+          src={logoPhonestore}
+          alt="Logo Phonestore"
+          title="Back to home"
+        />
+        <h1 className="header__heading--title">Phonestore</h1>
+      </Link>
+      <i className="fa fa-shopping-cart header__cart" aria-hidden="true">
+        <small className="header__cart--counter">{cartCount}</small>
       </i>
     </FlexBox>
   );
