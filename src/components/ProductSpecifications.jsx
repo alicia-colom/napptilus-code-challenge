2e73cbe03fd2d3289389d5d3533eddef38f4cac3
Alicia Colom <aliciacolom@gmail.com>
2022-07-23 19:55:59 +0200

    :bug: FIx camera info displayed in product details and Fix specification string

--- src/components/ProductSpecifications.jsx
+++ src/components/ProductSpecifications.jsx
@@ -1,8 +1,27 @@
 import React from 'react';
 import { FlexItem } from 'react-styled-flex';
 
-const ProductSpecifications = ({ mobileSpecifications, capitalize }) => {
+function ProductSpecifications({ mobileSpecifications, capitalize }) {
   const noToShowInDescription = ['id', 'brand', 'model', 'price', 'imgUrl'];
+  const isCameraInfo = Object.entries(mobileSpecifications).filter(
+    ([key]) =>
+      key && (key.includes('primaryCamera') || key.includes('secondaryCmera')),
+  );
+
+  const cameraContent = () =>
+    isCameraInfo.map((item, index) => {
+      const property = capitalize(item[0])
+        .split(/(?=[A-Z])/)
+        .join(' ');
+      const value = item[1].map((info) => <p>{info}</p>);
+
+      return (
+        <FlexItem as="li" key={index} box center alignItems="baseline">
+          <h5>{property}:</h5>
+          {value}
+        </FlexItem>
+      );
+    });
 
   const filteredMobileSpecificationsArr = Object.entries(
     mobileSpecifications,
@@ -10,13 +29,14 @@ const ProductSpecifications = ({ mobileSpecifications, capitalize }) => {
     ([key, value]) =>
       value &&
       typeof value === 'string' &&
-      key &&
       !noToShowInDescription.includes(key),
   );
 
-  const descriptionContent = filteredMobileSpecificationsArr.map(
-    (item, index) => {
-      const property = capitalize(item[0]);
+  const descriptionContent = () =>
+    filteredMobileSpecificationsArr.map((item, index) => {
+      const property = capitalize(item[0])
+        .split(/(?=[A-Z])/)
+        .join(' ');
       const value = item[1];
 
       return (
@@ -25,10 +45,13 @@ const ProductSpecifications = ({ mobileSpecifications, capitalize }) => {
           <p>{value}</p>
         </FlexItem>
       );
-    },
-  );
+    });
 
-  return descriptionContent;
-};
+  return (
+    <>
+      {cameraContent()} {descriptionContent()}
+    </>
+  );
+}
 
 export default ProductSpecifications;
