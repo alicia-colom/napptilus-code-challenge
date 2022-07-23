527298da101431300d7bcb4951e5da1cdb47c7e5
Alicia Colom <aliciacolom@gmail.com>
2022-07-23 13:45:06 +0200

    :iphone: + :truck: Move SearchBar into main component and Styled

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,16 +1,18 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import { FlexBox } from 'react-styled-flex';
+import SearchBar from './SearchBar';
 import logoPhonestore from '../assets/phonestore-alfa.png';
 import '../styles/Header.scss';
 
 function Header({ cartCount }) {
+  const [term, setTerm] = useState('');
   return (
     <FlexBox
       as="header"
       className="header"
       center
-      justifyContent="space-between"
+      justifyContent="space-around"
     >
       <Link to="/" className="header__heading">
         <img
@@ -21,9 +23,12 @@ function Header({ cartCount }) {
         />
         <h1 className="header__heading--title">Phonestore</h1>
       </Link>
-      <i className="fa fa-shopping-cart header__cart" aria-hidden="true">
-        <small className="header__cart--counter">{cartCount}</small>
-      </i>
+      <FlexBox center>
+        <SearchBar {...{ term, setTerm }} />
+        <i className="fa fa-shopping-cart header__cart" aria-hidden="true">
+          <small className="header__cart--counter">{cartCount}</small>
+        </i>
+      </FlexBox>
     </FlexBox>
   );
 }
--- src/views/CatalogueView.jsx
+++ src/views/CatalogueView.jsx
@@ -2,10 +2,9 @@ import React, { useState, useEffect } from 'react';
 import { FlexBox } from 'react-styled-flex';
 import { getCatalogue } from '../services/api';
 import { getItemFromLocalStorage } from '../services/storage';
-import { SearchBar, ProductCard } from '../components';
+import { ProductCard } from '../components';
 
 function CatalogueView({ setIsLoading, setMobileSpecifications }) {
-  const [term, setTerm] = useState('');
   const [initialList, setInitialList] = useState([]);
 
   useEffect(() => {
@@ -20,28 +19,23 @@ function CatalogueView({ setIsLoading, setMobileSpecifications }) {
   }, []);
 
   return (
-    <>
-      <FlexBox width="100%" justifyContent="flex-end" marginRight="30px">
-        <SearchBar {...{ term, setTerm }} />
-      </FlexBox>
-      <FlexBox
-        as="ul"
-        gap="24px"
-        width="872px"
-        maxWidth="90vw"
-        justifyContent="center"
-        alignContent="center"
-        wrap
-        padding="0"
-      >
-        {initialList.map((mobile) => (
-          <ProductCard
-            key={mobile.id}
-            {...{ mobile, setIsLoading, setMobileSpecifications }}
-          />
-        ))}
-      </FlexBox>
-    </>
+    <FlexBox
+      as="ul"
+      gap="24px"
+      width="872px"
+      maxWidth="90vw"
+      justifyContent="center"
+      alignContent="center"
+      wrap
+      padding="0"
+    >
+      {initialList.map((mobile) => (
+        <ProductCard
+          key={mobile.id}
+          {...{ mobile, setIsLoading, setMobileSpecifications }}
+        />
+      ))}
+    </FlexBox>
   );
 }
 
