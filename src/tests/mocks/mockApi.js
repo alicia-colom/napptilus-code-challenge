cb3c98908300f7061a99f3cecd2e0b43b658807b
Alicia Colom <aliciacolom@gmail.com>
2022-07-24 03:23:00 +0200

    :fire: Clean up mockApi

--- src/tests/mocks/mockApi.js
+++ src/tests/mocks/mockApi.js
@@ -97,65 +97,6 @@ const catalogueResponse = [
   },
 ];
 
-const productDetailsResponse = {
-  id: '8hKbH2UHPM_944nRHYN1n',
-  brand: 'Acer',
-  model: 'Liquid Z6',
-  price: '120',
-  imgUrl:
-    'https://front-test-api.herokuapp.com/images/8hKbH2UHPM_944nRHYN1n.jpg',
-  networkTechnology: 'GSM / HSPA / LTE',
-  networkSpeed: 'HSPA  LTE',
-  gprs: 'Yes',
-  edge: 'Yes',
-  announced: '2016  August',
-  status: 'Available. Released 2016  December',
-  dimentions: '-',
-  weight: '',
-  sim: ['Single SIM (Micro-SIM) or Dual SIM (Micro-SIM', 'dual stand-by)'],
-  displayType: 'IPS LCD capacitive touchscreen  16M colors',
-  displayResolution: '5.0 inches',
-  displaySize: '720 x 1280 pixels (~294 ppi pixel density)',
-  os: 'Android 6.0 (Marshmallow)',
-  cpu: 'Quad-core 1.25 GHz Cortex-A53',
-  chipset: 'Mediatek MT6737',
-  gpu: 'Mali-T720MP2',
-  externalMemory: 'microSD  up to 256 GB',
-  internalMemory: ['8 GB'],
-  ram: '1 GB RAM',
-  primaryCamera: ['8 MP', 'autofocus', 'LED flash'],
-  secondaryCmera: '2 MP',
-  speaker: 'Yes',
-  audioJack: 'Yes',
-  wlan: 'Yes',
-  bluetooth: 'Yes',
-  gps: 'Yes with A-GPS',
-  nfc: '',
-  radio: 'FM radio',
-  usb: 'microUSB 2.0',
-  sensors: ['Accelerometer', 'proximity'],
-  battery: 'Removable Li-Ion 2000 mAh battery',
-  colors: ['Black', 'White'],
-  options: {
-    colors: [
-      {
-        code: 1000,
-        name: 'Black',
-      },
-      {
-        code: 1001,
-        name: 'White',
-      },
-    ],
-    storages: [
-      {
-        code: 2000,
-        name: '8 GB',
-      },
-    ],
-  },
-};
-
 export default async function mockApi(url) {
   switch (url) {
     case 'https://front-test-api.herokuapp.com/api/product': {
@@ -163,14 +104,6 @@ export default async function mockApi(url) {
         ok: true,
         status: 200,
         json: async () => catalogueResponse,
-      };
-    }
-
-    case 'https://front-test-api.herokuapp.com/api/product/8hKbH2UHPM_944nRHYN1n': {
-      return {
-        ok: true,
-        status: 200,
-        json: async () => productDetailsResponse,
       };
     }
 
